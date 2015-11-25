d792e6f72f0df6022d70aaf611ebb5adffcdd17f
queile <reussir.bien@gmail.com>
2015-11-25 17:04:39 +0900

    Add place

--- scrape_stereo.rb
+++ scrape_stereo.rb
@@ -13,22 +13,27 @@ end
 #date = nil
 
 doc = Nokogiri::HTML.parse(html, nil, charset)
-doc.xpath('//div[@class="s-item-title"]//h3').each do |node|
-	#node.xpath('div').each do |div|
+doc.xpath('//div[contains(@class, "s-item-text-group")]').each do |group|
+	group.xpath('div[@class="s-item-title"]//h3').each do |node|
 		text = node.inner_text
-		#if date
-		#	p node.inner_text
-		#	p date
-		#	date = nil
-		#	next
-		#end
 		begin
 			date = Time.parse(text)
 			title = node.xpath('div')[1].inner_text rescue text
 			p title
 			p date
+
+			place_flag = false
+			group.xpath('div[contains(@class, "s-item-text")]//div').each_with_index do |s_item, i|
+				if place_flag
+					s_item_innner = s_item.xpath('div')
+					place = s_item_innner[0].inner_text rescue s_item.inner_text
+					p place 
+					break
+				end
+				place_flag = true if s_item.inner_text == "Place"
+			end
 		rescue
 
 		end
-	#end
+	end
 end
